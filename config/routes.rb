0a404a8be6bb54ca24ebd678eeb0be9da8eadd1b
HomareYamamoto <159757552+HomareYamamoto@users.noreply.github.com>
2024-05-06 04:01:05 +0000

    ログイン機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,23 @@
 Rails.application.routes.draw do
+
+  devise_for :admins, controllers: {
+  sessions: "admin/sessions"
+  }
+
+  devise_for :users, controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+  }
+
+  root to: 'public/homes#top'
+
+  namespace :public do
+    get 'homes/about'
+  end
+
+  namespace :admin do
+    get '/' => 'homes#top'
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

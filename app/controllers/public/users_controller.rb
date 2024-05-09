b36bcd42cf1dd6c17cac9c38de79404b678b4c56
HomareYamamoto <159757552+HomareYamamoto@users.noreply.github.com>
2024-05-09 16:06:57 +0000

    ユーザー一覧・詳細表示実装

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,26 +1,58 @@
 class Public::UsersController < ApplicationController
+  # before_action :authenticate_customer!
+  before_action :check_user_existence, only: [:show, :edit, :update]
 
   def my_page
     @user=current_user
+    @post_players=@user.post_players
   end
 
 
   def index
+    @users=User.all
   end
 
   def show
+    @user=User.find(params[:id])
+    @post_players=@user.post_players
   end
 
   def edit
+    @user=current_user
   end
 
   def update
+    @user=User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to public_user_path(@user.id), notice: "You have updated user successfully."
+    else
+      render :edit
+    end
   end
 
   def unsubscribe
   end
 
   def withdraw
+    @user=User.find(current_user.id)
+    @user.update(is_active: false)
+    reset_session
+    flash[:notice] = "退会処理を実行いたしました"
+    redirect_to root_path
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
+
+  def check_user_existence
+      @user=User.find_by(id: params[:id])
+      unless @user
+        redirect_to public_users_path
+      end
   end
 
 end

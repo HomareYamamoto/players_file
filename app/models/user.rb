7f222ff885923a7e2dae584d3e1cb912a5bf7d06
HomareYamamoto <159757552+HomareYamamoto@users.noreply.github.com>
2024-05-10 10:02:44 +0000

    フォロー機能エラー

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,8 +9,29 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
 
 
+  has_many :relationships, class_name: "Relationship", foreign_key: "follow_id", dependent: :destroy
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+
+  has_many :followings, through: :relationships, source: :follower
+  has_many :followers, through: :reverse_of_relationships, source: :follow
+
+
   def active_for_authentication?
     super && (is_active == true)
   end
+
+  # フォローしたときの処理
+  def follow(user_id)
+    relationships.create(follow_id: user_id)
+  end
+  # フォローを外すときの処理
+  def unfollow(user_id)
+    relationships.find_by(follow_id: user_id).destroy
+  end
+  # フォローしているか判定
+  def following?(user)
+    followings.include?(user)
+  end
+
 
 end

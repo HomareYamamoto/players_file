c3c95a450f809fff1aafb235cf67d69708e48f80
HomareYamamoto <159757552+HomareYamamoto@users.noreply.github.com>
2024-05-10 06:37:12 +0000

    コメント機能実装

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -2,9 +2,23 @@ class Public::CommentsController < ApplicationController
 
 
   def create
+    @post_player = PostPlayer.find(params[:post_player_id])
+    comment = current_user.comments.new(comment_params)
+    comment.post_player_id = @post_player.id
+    comment.save
+    redirect_to request.referer
   end
 
   def destroy
+    @post_player=PostPlayer.find(params[:post_player_id])
+    Comment.find(params[:id]).destroy
+    redirect_to request.referer
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body)
   end
 
 end
--- app/controllers/public/post_players_controller.rb
+++ app/controllers/public/post_players_controller.rb
@@ -22,6 +22,7 @@ class Public::PostPlayersController < ApplicationController
 
   def show
     @post_player=PostPlayer.find(params[:id])
+    @comment=Comment.new
   end
 
   def edit

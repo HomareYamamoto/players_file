8ead99bda0f25d15743f7bfab48ef0453bfadb18
HomareYamamoto <159757552+HomareYamamoto@users.noreply.github.com>
2024-06-27 09:13:43 +0000

    コメント通知機能追加

--- app/helpers/public/notifications_helper.rb
+++ app/helpers/public/notifications_helper.rb
@@ -3,10 +3,10 @@ module Public::NotificationsHelper
     case notification.notifiable_type
     when "PostPlayer"
       "フォローしている#{notification.notifiable.user.name}さんが#{notification.notifiable.name}を投稿しました"
-    # when "Comment"
-    #   "自身の投稿にコメントがつきました" コメントに通知機能を作成予定
-    else
+    when "Favorite"
       "投稿した#{notification.notifiable.post_player.name}が#{notification.notifiable.user.name}さんにいいねされました"
+    when "Comment"
+      "投稿した#{notification.notifiable.post_player.name}にコメントがつきました"
     end
   end
 end

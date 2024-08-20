3233a22268827a8528c147826f471509c78a814e
HomareYamamoto <159757552+HomareYamamoto@users.noreply.github.com>
2024-08-20 04:23:41 +0000

    ページトップスクロール機能実装

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,9 +13,9 @@ import "popper.js";
 import "bootstrap";
 import "../stylesheets/application";
 import 'jscroll';
-import '../stylesheets/public/post_players/index.css';
-import '../packs/public/post_players/script.js';
-import '../packs/public/post_players/jquery.min.js';
+// import '../stylesheets/public/post_players/index.css';
+import 'script.js';
+// import '../packs/public/post_players/jquery.min.js';
 
 Rails.start()
 Turbolinks.start()
